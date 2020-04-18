21ca5338a94b5e4cb1d5a33f4bd1c2484d159a1e
Teo_Hwang <taeung2008@gmail.com>
2020-04-19 01:53:15 +0900

    add robots.txt and del sitemap because too hard install sitemap

--- nickname/urls.py
+++ nickname/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('result/<int:pk>/', views.result_name, name='result_name'),
     path('', views.select_catagory, name='select_catagory'),
     path('', views.throw, name='throw'),
     path('catch/', views.catch, name='catch'),
+    path('robots.txt/', TemplateView.as_view(template_name="nickname/robots.txt", content_type="text/plain"), name="project_robots_file"),
 ]
 
